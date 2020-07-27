a080d214e815c6c2579caafae4e9bb530bc92162
daiomg12 <dai.ohno.1212@gmail.com>
2020-07-27 21:41:50 +0900

    Add self_introduction on profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction])
     end
 end
