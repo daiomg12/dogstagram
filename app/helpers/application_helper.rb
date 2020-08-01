a4f39bc15e18dc23d7ea4f60b7423b186052cd36
daiomg12 <dai.ohno.1212@gmail.com>
2020-08-01 18:46:26 +0900

    Fix error for avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    return user.profile_photo unless user.profile_photo.nil?
+    return user.profile_photo.medium.url unless user.profile_photo.medium.url.blank?
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
   end
